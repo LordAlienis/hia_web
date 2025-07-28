3dea21e6caf520d68a8ecf35d7957d96839b6f53
Daniel Enrique Astudillo Guzmán <49796899+LordAlienis@users.noreply.github.com>
2025-07-28 10:41:36 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Navbar from './components/Navbar'
-import './index.css'
-import { AnimatePresence } from 'framer-motion'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
-      </AnimatePresence>
-    </Router>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
