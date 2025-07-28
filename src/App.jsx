d7b75e8e9f0d5c768233a754028f94b53fe7ae9a
Daniel Enrique Astudillo Guzmán <49796899+LordAlienis@users.noreply.github.com>
2025-07-28 10:40:51 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </AnimatePresence>
+    </Router>
+  );
+}
